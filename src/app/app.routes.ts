2147667ebf10de0d656e2daa52a6c9a304cf9cd3
Ingriddy01 <ingriddy.santos02@gmail.com>
2026-06-02 15:49:51 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,9 @@
 import { Routes } from '@angular/router';
-import { SuserFormComponent } from './components/suser-form/suser-form.component';
+import { UserFormComponent } from './components/suser-form/user-form.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: SuserFormComponent
-  },
-  {
-    path: '**',
-    redirectTo: ''
+    component: UserFormComponent
   }
 ];
