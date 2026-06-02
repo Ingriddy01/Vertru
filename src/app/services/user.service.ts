2fa830ba16416b88d4958ea861a0fb94071be024
Ingriddy01 <ingriddy.santos02@gmail.com>
2026-06-02 16:12:52 -0300

    Update user.service.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -9,8 +9,9 @@ export class UserService {
   private userSource = new BehaviorSubject<UserData | null>(null);
   user$ = this.userSource.asObservable();
 
-  salvarUsuario(user: UserData): void {
-    this.userSource.next(user);
-    console.log('Usuário salvo com sucesso:', user);
+  this.userService.salvarUsuario(dados);
+
+this.dadosParaExibir = dados;
+this.displayModal = true;
   }
 }
